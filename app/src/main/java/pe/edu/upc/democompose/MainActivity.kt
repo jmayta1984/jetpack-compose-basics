04efc804e191c9506d707cfb5af8cc0fa93054d4
jmayta1984 <jmayta@pucp.edu.pe>
2021-08-28 14:41:58 -0500

    Jetpack Compose Basics

--- app/src/main/java/pe/edu/upc/democompose/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/democompose/MainActivity.kt
@@ -3,19 +3,23 @@ package pe.edu.upc.democompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.setValue
 import pe.edu.upc.democompose.ui.theme.DemoComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,9 +44,8 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it"}) {
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" }) {
     val counterState = remember { mutableStateOf(0) }
-
     Column {
         NameList(names, Modifier.weight(1f))
         Counter(
@@ -57,7 +60,7 @@ fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it"}) {
 @Composable
 fun NameList(names: List<String>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
-        items (items = names){ name ->
+        items(items = names) { name ->
             Greeting(name = name)
             Divider(color = Color.Black)
         }
@@ -78,7 +81,15 @@ fun Counter(count: Int, updateCount: (Int) -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .padding(24.dp)
+            .background(color = backgroundColor)
+            .clickable(onClick = { isSelected = !isSelected })
+    )
 }
 
 @Preview(name = "Text preview")
